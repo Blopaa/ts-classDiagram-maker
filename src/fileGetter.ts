9606637931efa2caf57e482cd238945b76339c25
Pablo Sabater <pabloosabaterr@gmail.com>
2021-03-12 17:01:10 +0100

    refactor: fileGetter.ts

--- src/fileGetter.ts
+++ src/fileGetter.ts
@@ -1,10 +1,12 @@
 import {PathLike, readdirSync} from 'fs'
 import * as fs from "fs";
 import {classRegex} from "../types/regex";
+import ClassEntity from "./entities/class";
+import FileEntity from "./entities/file";
 
 export default class FileGetter {
 
-    private path: PathLike;
+    private readonly path: PathLike;
 
     constructor(path: PathLike) {
         this.path = path;
@@ -14,11 +16,15 @@ export default class FileGetter {
         if (this.path) {
 
             const files: String[] = readdirSync(this.path.toString()).filter(e => e.endsWith(".ts"));
-            let data: String[] = [];
+            let data: FileEntity[] = [];
             for (let file of files) {
-                data.push(...fs.readFileSync(`${this.path}/${file}`, 'utf-8').toString().match(classRegex).map(e => (
-                    e.split(" ")[1]
-                )));
+                let newFile = new FileEntity();
+                fs.readFileSync(`${this.path}/${file}`, 'utf-8').toString().match(classRegex).map(e => {
+                    let newClass = new ClassEntity();
+                    newClass.name = e.split(" ")[1];
+                    newFile.clases.push(newClass);
+                });
+                data.push(newFile);
             }
             console.log(data)
         } else {
--- types/regex/index.ts
+++ types/regex/index.ts
@@ -1,1 +1,2 @@
-export const classRegex: RegExp = new RegExp(/class.[A-Za-z]{1,}/g);
+export const classRegex: RegExp = new RegExp(/class [A-Za-z]{1,}/g);
+export const extendRegex: RegExp = new RegExp(/extends [A-Za-z]{1,}/g);
